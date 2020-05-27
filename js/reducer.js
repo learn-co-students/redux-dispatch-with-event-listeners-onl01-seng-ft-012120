64c18089f6b3e585c782d24ec983c8b8910d0e20
dwinniford <dwinniford@gmail.com>
2020-05-27 09:42:51 -0500

    Done.

--- js/reducer.js
+++ js/reducer.js
@@ -1,1 +1,31 @@
 // add code snippets from README
+let state 
+
+function reducer(state = {count: 0}, action) {
+    switch (action.type) {
+        case "INCREASE_COUNT":
+            return {count: state.count + 1}
+        default:
+            return state
+    }
+}
+
+const container = document.querySelector("#container")
+
+function render() {
+    container.innerHTML = state.count
+}
+
+function dispatch(action) {
+    state = reducer(state, action)
+    render()
+}
+
+dispatch('@@INIT')
+
+const button = document.querySelector('#button')
+button.addEventListener("click", function(event) {
+    console.log('state')
+    dispatch({type: "INCREASE_COUNT"})
+})
+
